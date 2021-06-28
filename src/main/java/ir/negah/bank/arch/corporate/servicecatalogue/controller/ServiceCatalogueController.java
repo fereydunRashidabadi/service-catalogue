d378c9ed8e43159ce3ee5ecd958d7c64c6e5fb28
JiraAdmin <atlassian@karafarinbank.ir>
2021-06-28 09:24:24 +0430

    feature, نسخه استیبل ورژن 1.0.0

--- src/main/java/ir/negah/bank/arch/corporate/servicecatalogue/controller/ServiceCatalogueController.java
+++ src/main/java/ir/negah/bank/arch/corporate/servicecatalogue/controller/ServiceCatalogueController.java
@@ -1,43 +1,37 @@
 package ir.negah.bank.arch.corporate.servicecatalogue.controller;
 
-import io.swagger.annotations.*;
-import ir.negah.bank.arch.corporate.servicecatalogue.domain.dto.*;
-import ir.negah.bank.arch.util.pojo.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
-
 /**
- * created by  f.rashidabadi
- * created on 6/19/2021
- * inside the package - ir.negah.bank.arch.arch.corporate.servicecatalogue.controller
- **/
-
+ * created by f.rashidabadi created on 6/19/2021 inside the package -
+ * ir.negah.bank.arch.arch.corporate.servicecatalogue.controller
+ */
 @Controller
 public class ServiceCatalogueController {
-    @GetMapping("/index")
-    public String index(Model model) {
-        return "index-new";
-    }
-    @GetMapping("/welcome")
-    public String welcome(Model model) {
-        return "welcome";
-    }
+  @GetMapping("/index")
+  public String index(Model model) {
+    return "index";
+  }
 
-    @GetMapping("/company")
-    public String company(Model model) {
-        return "company";
-    }
+  @GetMapping("/welcome")
+  public String welcome(Model model) {
+    return "welcome";
+  }
 
-    @GetMapping("/project")
-    public String project(Model model) {
-        return "project";
-    }
+  @GetMapping("/company")
+  public String company(Model model) {
+    return "company";
+  }
 
-    @GetMapping("/endpoint")
-    public String endpoint(Model model) {
-        return "endpoint";
-    }
+  @GetMapping("/project")
+  public String project(Model model) {
+    return "project";
+  }
+
+  @GetMapping("/endpoint")
+  public String endpoint(Model model) {
+    return "endpoint";
+  }
 }
